6c46fe213de24443ef8bc840065a5ed1413c9499
Touhidul Shawan <touhidulislamshaon@gmail.com>
2019-07-16 17:26:27 +0600

    Updates

---
+++ precache-manifest.843d9ea527c74a9427b8db6de8a9ca1b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52526f94fec5bcb5abc682de6e5e8046",
+    "url": "/songs-app/index.html"
+  },
+  {
+    "revision": "7cb129d0d218ac5f0ea7",
+    "url": "/songs-app/static/css/main.c6c89b66.chunk.css"
+  },
+  {
+    "revision": "6bad44b7a7b8c9c1b238",
+    "url": "/songs-app/static/js/2.bab56663.chunk.js"
+  },
+  {
+    "revision": "7cb129d0d218ac5f0ea7",
+    "url": "/songs-app/static/js/main.ee620747.chunk.js"
+  },
+  {
+    "revision": "096bde1159209503dd51",
+    "url": "/songs-app/static/js/runtime~main.99a053a3.js"
+  },
+  {
+    "revision": "dcdbca7c20ef8303d5498aac0a5b6b0a",
+    "url": "/songs-app/static/media/Camila-Cabello-Shawn-Mendes-Señorita-Behind--Scenes.dcdbca7c.jpg"
+  },
+  {
+    "revision": "9b341777a990d5e621d97910819eb2b6",
+    "url": "/songs-app/static/media/Hold Me While You Wait.9b341777.jpg"
+  },
+  {
+    "revision": "7e800b1619d9a3ba20f38de57fb37b0e",
+    "url": "/songs-app/static/media/beautiful_people.7e800b16.jpg"
+  },
+  {
+    "revision": "55fe6ab2b76c57ce75e66d414e57121e",
+    "url": "/songs-app/static/media/idon`tCare.55fe6ab2.png"
+  },
+  {
+    "revision": "639172a021d842bd6f07e348e978df80",
+    "url": "/songs-app/static/media/old_town_road.639172a0.jpeg"
+  }
+]);
